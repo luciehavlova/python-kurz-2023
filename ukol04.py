00f139a8eb4b892d0221ddda5895b3b7fefb9f52
Lucie Havlova <lucie9@centrum.cz>
2023-02-16 18:14:27 +0100

    Úkol 04 - doplnění

--- ukol04.py
+++ ukol04.py
@@ -18,3 +18,18 @@ def cena_zpravy(zprava):
 zprava = input("Zadej textovou zprávu:")
 
 # print (f"Cena za odeslání SMS na číslo {telefonni_cislo} je {cena_zpravy} Kč.")
+
+# opravená varianta:
+cislo = input("Zadej telefonní číslo s předvolbou:")
+
+def zasilani_sms(overeni_cisla)->int:
+    return len (overeni_cisla)==9 or (len(overeni_cisla)==12 and overeni_cisla[:4]=="+420")
+
+zprava = input("Zadej textovou zprávu:")
+
+def cena_zpravy(cena):
+    return cena 
+cena = len(zprava) * 3 /180
+
+ 
+print (f"Cena za odeslání SMS na číslo {cislo} je {cena} Kč.")
